6d2931d53d53b8d1d92b47afdca95819abd4377b
AmelieBarbier <amliedu28@gmail.com>
2026-06-11 18:28:41 +0200

    Complete reservation CRUD

---
+++ routes/reservations.js
@@ -0,0 +1,54 @@
+const Reservation = require("../models/Reservation");
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+
+    const reservations = await Reservation.find();
+
+    res.json(reservations);
+
+});
+
+router.get("/:id", async (req, res) => {
+
+    const reservation = await Reservation.findById(
+        req.params.id
+    );
+
+    res.json(reservation);
+
+});
+
+router.post("/", async (req, res) => {
+
+    const reservation = new Reservation(req.body);
+
+    await reservation.save();
+
+    res.json(reservation);
+
+});
+
+router.put("/:id", async (req, res) => {
+
+    const reservation = await Reservation.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.json(reservation);
+
+});
+
+router.delete("/:id",async (req,res) => {
+    const reservation =await Reservation.findByIdAndDelete (
+        req.params.id
+    );
+    res.json(reservation);
+});
+
+
+module.exports = router;
